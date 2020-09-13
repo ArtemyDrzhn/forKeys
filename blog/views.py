6759b8ed5184fd90c0a9ef0784fff4c061ff8376
ArtemyDrzhn <artemdrzhn@gmail.com>
2020-09-13 11:51:35 +0300

    The first commit in project forKeys

--- blog/views.py
+++ blog/views.py
@@ -1,18 +1,2 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
-from .models import Users
-
-
-# получение данных из бд
-def index(request):
-    user = Users.objects.all()
-    return render(request, "index.html", {"user": user})
-
-
-# сохранение данных в бд
-def create(request):
-    if request.method == "POST":
-        tom = Users()
-        tom.user = request.POST.get("user")
-        tom.save()
-    return HttpResponseRedirect("/")
