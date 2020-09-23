7504c7edd1a991efa77b1ed0969c7f02d2840596
Druzhinin Artemiy <artemdrzhn@gmail.com>
2020-09-23 10:28:28 +0300

    two users and variable name changes

--- blog/admin.py
+++ blog/admin.py
@@ -1,12 +1,13 @@
 from django.contrib import admin
 from .models import Questions
-from .models import Users
+from .models import Persons
+from django.contrib.auth.models import User
 from .models import Answers
 from .models import Comments
 from .models import Categories
 
 admin.site.register(Questions)
-admin.site.register(Users)
+admin.site.register(Persons)
 admin.site.register(Answers)
 admin.site.register(Comments)
 admin.site.register(Categories)
--- blog/models.py
+++ blog/models.py
@@ -1,27 +1,53 @@
 from django.conf import settings
 from django.db import models
-from django.utils import timezone
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
+class Persons(models.Model):
+    name_person=  models.CharField(max_length=30)
+    is_manager = models.BooleanField(default=False)
 
+    @receiver(post_save, sender=User)
+    def create_user_persons(sender, instance, created, **kwargs):
+        if created:
+            Persons.objects.create(user=instance)
 
-class Users(models.Model):
-    user=  models.CharField(max_length=30)
-    is_manager = models.BooleanField(default=True)
+    @receiver(post_save, sender=User)
+    def save_user_profile(sender, instance, **kwargs):
+        instance.persons.save()
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
 
-class Questions(models.Model):
-    user = models.ForeignKey(Users, on_delete=models.CASCADE)
-    text = models.TextField(max_length=300)
-
-class Answers(models.Model):
-    question = models.OneToOneField(Questions, on_delete=models.CASCADE, primary_key=True)
-    user = models.ForeignKey(Users, on_delete=models.CASCADE)
-    answer = models.CharField(max_length=300)
-
-class Comments(models.Model):
-    question = models.ForeignKey(Questions, on_delete=models.CASCADE)
-    user = models.ForeignKey(Users, on_delete=models.CASCADE)
-    text = models.TextField(max_length=300)
 
 class Categories(models.Model):
-    question = models.ManyToManyField(Questions)
-    categories = models.CharField(max_length=300)
+    name_cat = models.CharField(max_length=300)
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+class Questions(models.Model):
+    name_person = models.ForeignKey(Persons, on_delete=models.CASCADE)
+    text = models.TextField(max_length=1024)
+    name_cat = models.ForeignKey(Categories, on_delete=models.CASCADE)
+    class Meta:
+        verbose_name = 'Вопрос'
+        verbose_name_plural = 'Вопросы'
+
+class Answers(models.Model):
+    ask = models.OneToOneField(Questions, on_delete=models.CASCADE, primary_key=True)
+    name_person = models.ForeignKey(Persons, on_delete=models.CASCADE)
+    text = models.CharField(max_length=1024)
+    class Meta:
+        verbose_name = 'Ответ'
+        verbose_name_plural = 'Ответы'
+
+class Comments(models.Model):
+    ask = models.ForeignKey(Questions, on_delete=models.CASCADE)
+    name_person = models.ForeignKey(Persons, on_delete=models.CASCADE)
+    text = models.TextField(max_length=1024)
+    class Meta:
+        verbose_name = 'Комментарий'
+        verbose_name_plural = 'Комментарии'
+
